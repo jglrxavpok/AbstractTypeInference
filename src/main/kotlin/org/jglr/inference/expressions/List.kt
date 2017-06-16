0940333fe508c962e95daebfc7fe0de04d2b1053
jglrxavpok <jglrxavpok@gmail.com>
2017-06-16 21:23:18 +0200

    Small fix -> trying to reduce empty lists (again)

--- src/main/kotlin/org/jglr/inference/expressions/List.kt
+++ src/main/kotlin/org/jglr/inference/expressions/List.kt
@@ -4,5 +4,5 @@ import kotlin.collections.List
 
 class List(val elements: List<Expression>) : Expression() {
     override val stringRepresentation: String
-        get() = "["+elements.map(Expression::stringRepresentation).reduce {a, b -> "$a; $b"}+"]"
+        get() = if(elements.isEmpty()) "[]" else "["+elements.map(Expression::stringRepresentation).reduce {a, b -> "$a; $b"}+"]"
 }
