92f2bedd9563d8fe0c3f917c67ec1a3ff64e8370
jglrxavpok <jglrxavpok@gmail.com>
2017-06-16 21:20:20 +0200

    Small fix -> trying to reduce empty lists

--- src/main/kotlin/org/jglr/inference/expressions/Tuple.kt
+++ src/main/kotlin/org/jglr/inference/expressions/Tuple.kt
@@ -2,5 +2,5 @@ package org.jglr.inference.expressions
 
 class Tuple(vararg val arguments: Expression) : Expression() {
     override val stringRepresentation: String
-        get() = "("+arguments.map(Expression::stringRepresentation).reduce {a, b -> "$a, $b"}+")"
+        get() = if (arguments.isEmpty()) "()" else "("+arguments.map(Expression::stringRepresentation).reduce {a, b -> "$a, $b"}+")"
 }
